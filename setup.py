c36957d1b329b6891ab503b67cb636dc664edb38
quaghate <rsabelha@gmail.com>
2024-12-07 17:48:18 -0300

    bug fix setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
+import pathlib
+
+# Diretório atual
+HERE = pathlib.Path(__file__).parent
+
+# Conteúdo do arquivo README
+README = (HERE / "README.md").read_text()
 
 setup(
     name='aicluster',
-    version='1.0.0-a1',
+    version='1.0.0a1',
     packages=find_packages(),
     install_requires=[
         'numpy',
@@ -21,6 +28,8 @@ setup(
     author='Quaghate',
     author_email='rsabelha@gmail.com',
     description='Um miniframework para facilitar a criação e treinamento de IAs',
+    long_description=README,  # Adiciona a descrição longa a partir do README.md
+    long_description_content_type='text/markdown',  # Especifica o tipo de conteúdo do README
     url='https://github.com/seuusuario/aicluster',
     classifiers=[
         'Programming Language :: Python :: 3',
@@ -29,4 +38,3 @@ setup(
     ],
     python_requires='>=3.6',
 )
-
